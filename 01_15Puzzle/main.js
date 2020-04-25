c6290a038d7b445c421d17db73b848bcced64239
fumio68 <rgkk.5123@gmail.com>
2020-04-25 22:06:03 +0900

    25マスに変更(完成)

--- 01_15Puzzle/main.js
+++ 01_15Puzzle/main.js
@@ -7,12 +7,12 @@ let tiles = [];
 // 初期化関数
 function init(){
   let table = document.querySelector("table");
-  
-  for(let i=0; i < 4; i++) {
+
+  for(let i=0; i < 5; i++) {
     let tr = document.createElement("tr");
-    for(let j = 0;j < 4; j++) {
+    for(let j = 0;j < 5; j++) {
       let td = document.createElement("td");
-      let index = i * 4 + j;
+      let index = i * 5 + j;
       td.className = "tile";
       td.index = index;
       td.value = index;
@@ -23,26 +23,32 @@ function init(){
     }
     table.appendChild(tr);
   }
-  
-  for(var i = 0;i < 1000; i++){
-    click({srcElement: {index:Math.floor(Math.random()*16)}})
+
+  for(let i = 0;i < 1000; i++){
+    click({srcElement: {index:Math.floor(Math.random()*25)}})
   }
 }
 
 function click(e){
   let i = e.srcElement.index;
 
-  if(i-4 >= 0 && tiles[i-4].value == 0) {
-    swap(i, i-4);
-  }else if(i+4 < 16 && tiles[i+4].value == 0) {
-    swap(i, i+4);
-  }else if(i%4 != 3 && tiles[i-1].value == 0) {
+  if(i-5 >= 0 && tiles[i-5].value == 0) {
+    swap(i, i-5);
+  }else if(i+5 < 25 && tiles[i+5].value == 0) {
+    swap(i, i+5);
+  }else if(i%5 != 0 && tiles[i-1].value == 0) {
     swap(i, i-1);
-  }else if(i%4 != 3 && tiles[i+1].value == 0) {
+  }else if(i%5 != 4 && tiles[i+1].value == 0) {
     swap(i, i+1)
   }
 }
 
-
+function swap(i,j){
+  let tmp = tiles[i].value;
+  tiles[i].textContent = tiles[j.textContent];
+  tiles[i].value = tiles[j].value;
+  tiles[j].textContent = tmp;
+  tiles[j].value = tmp;
+}
 
 init();
