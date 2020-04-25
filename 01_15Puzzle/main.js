4451224cf8b6ddc504abe7428ba5ee9894eaaef4
fumio68 <rgkk.5123@gmail.com>
2020-04-25 18:56:58 +0900

    click関数の作成

--- 01_15Puzzle/main.js
+++ 01_15Puzzle/main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-init();
 // 広域変数
 let tiles = [];
 
@@ -8,7 +7,7 @@ let tiles = [];
 // 初期化関数
 function init(){
   let table = document.querySelector("table");
-
+  
   for(let i=0; i < 4; i++) {
     let tr = document.createElement("tr");
     for(let j = 0;j < 4; j++) {
@@ -24,10 +23,26 @@ function init(){
     }
     table.appendChild(tr);
   }
-
+  
   for(var i = 0;i < 1000; i++){
     click({srcElement: {index:Math.floor(Math.random()*16)}})
   }
 }
 
+function click(e){
+  let i = e.srcElement.index;
 
+  if(i-4 >= 0 && tiles[i-4].value == 0) {
+    swap(i, i-4);
+  }else if(i+4 < 16 && tiles[i+4].value == 0) {
+    swap(i, i+4);
+  }else if(i%4 != 3 && tiles[i-1].value == 0) {
+    swap(i, i-1);
+  }else if(i%4 != 3 && tiles[i+1].value == 0) {
+    swap(i, i+1)
+  }
+}
+
+
+
+init();
