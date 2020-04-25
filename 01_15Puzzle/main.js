6b050b02c617c95b316c5a42ba3cb664a9c38e1b
fumio68 <rgkk.5123@gmail.com>
2020-04-25 18:47:12 +0900

    init関数の作成

--- 01_15Puzzle/main.js
+++ 01_15Puzzle/main.js
@@ -1,9 +1,33 @@
 "use strict";
 
+init();
 // 広域変数
-var tiles = [];
+let tiles = [];
+
 
 // 初期化関数
 function init(){
-  
+  let table = document.querySelector("table");
+
+  for(let i=0; i < 4; i++) {
+    let tr = document.createElement("tr");
+    for(let j = 0;j < 4; j++) {
+      let td = document.createElement("td");
+      let index = i * 4 + j;
+      td.className = "tile";
+      td.index = index;
+      td.value = index;
+      td.textContent = index == 0? "":index;
+      td.onclick = click;
+      tr.appendChild(td);
+      tiles.push(td);
+    }
+    table.appendChild(tr);
+  }
+
+  for(var i = 0;i < 1000; i++){
+    click({srcElement: {index:Math.floor(Math.random()*16)}})
+  }
 }
+
+
